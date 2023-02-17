5f3cc255d8618b0eebdbcbb6117874e0f37d1164
Jinhjlog <dndn2615@gmail.com>
2023-02-17 17:58:50 +0900

    socketIO-0217

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,57 +1,42 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-// app.js의 socket은, 서버로의 연결을 뜻한다.
-const socket = new WebSocket(`ws://${window.location.host}`);
+// io는 자동적으로 back end socket.io와 연결해주는 함수
+const socket = io();
 
-function makeMessage(type, payload) {
-    const msg = {type, payload}
-    return JSON.stringify(msg);
-}
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-function handleOpen(){
-    console.log("Connected to Server")
-}
+room.hidden = true;
 
-// 이 부분은 socket이 connection을 open 했을 때 발생함
-socket.addEventListener("open", handleOpen)
+let roomName;
 
-// 메세지를 서버에서 받았을 때 
-socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data, " from the server");
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-
-
-// 서버와의 연결이 끊겼을 때
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server X");
-});
-
-function handleSubmit(event){
-    // preventDefault() 
-    // 1. a 태그를 눌렀을때도 href 링크로 이동하지 않게 할 경우
-    // 2. form 안에 submit 역할을 하는 버튼을 눌렀어도 새로 실행하지 않게 하고싶을 경우(submit은 작동 됨)
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    console.log(input.value);
-
-    const li = document.createElement("li");
-    li.innerText = `You : ${input.value}`;
-    messageList.append(li);
-
-    input.value="";
-}
-
-function handleNickSubmit(event){
-    event.preventDefault()
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
+function addText(){
     
 }
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+
+function showRoom(){
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerHTML = `Room ${roomName}`;
+}
+
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    
+    // 첫 번째 argument에는 evnet 이름이 들어감
+    // 두 번째 argument에는 보내고 싶은 payload
+    // 세 번째 argument에는 서버에서 호출하는 function이 들어감
+    socket.emit(
+        "enter_room", 
+        input.value,
+        showRoom
+        );
+    
+    roomName = input.value;
+    input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import http from "http"
-import WebSocket from "ws";
+import http from "http";
+import {Server} from "socket.io";
 import express from "express";
 
 const app = express();
@@ -14,21 +14,39 @@ app.get("/", (req, res) => res.render("home"));
 // 어떤 홈페이지로 가던지 home으로 이동
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
+// http서버 생성
+const httpServer = http.createServer(app);
+// socketIO 서버 생성
+const wsServer = new Server(httpServer);
 
+wsServer.on("connection", socket => {
+    socket.onAny((event) => {
+        console.log(`Socket Event : ${event}`);
+    })
+    socket.on("enter_room", (roomName, done) => {
+        // 소켓 그룹을 따로 만든다 (예 : 채팅방)
+        socket.join(roomName);
+        done();
+        
+        socket.to(roomName).emit("welcome");
+        // 백엔드에서 함수를 실행시키는게 아님
+        // 백엔드에서 진행할 경우 보안에 문제가 생김
+    });
+})
 
 // server.js에 있는 socket은 연결된 브라우저를 뜻함
 // function handleConnection(socket) {
 //     console.log(socket);
 
 // } 
-const wss = new WebSocket.Server({ server });
 // on 메소드는 event가 동작하는것을 기다린다.
 // on 메소드는 backend에 연결된 사람의 정보를 제공해줌
 // socket에서 온다
 // socket 은 서버와 브라우저 사이의 연결
+/*
+const wss = new WebSocket.Server({ server });
+
 const sockets = [];
 
 wss.on("connection", (socket) => {
@@ -45,21 +63,18 @@ wss.on("connection", (socket) => {
 
         switch(message.type){
             case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.pa}`))
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`))
                 break;
             case "nickname":
                 socket["nickname"] = message.payload;
         }
     });
 })
-
-function onCloseSocket(){
-    console.log("Disconnected from Browser X");
-}
+*/
 
 
-
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
 
 
 
