acf7a6773eef94843fc4f6d4e9832903694d6a6c
Jinhjlog <dndn2615@gmail.com>
2023-02-17 14:51:56 +0900

    webSocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,57 @@
-
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 // app.js의 socket은, 서버로의 연결을 뜻한다.
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-// 이 부분은 socket이 connection을 open 했을 때 발생함
-socket.addEventListener("open", () => {
+function makeMessage(type, payload) {
+    const msg = {type, payload}
+    return JSON.stringify(msg);
+}
+
+function handleOpen(){
     console.log("Connected to Server")
-})
+}
 
+// 이 부분은 socket이 connection을 open 했을 때 발생함
+socket.addEventListener("open", handleOpen)
+
+// 메세지를 서버에서 받았을 때 
 socket.addEventListener("message", (message) => {
-    console.log("Just got this: ", message, " from the server");
+    console.log("New message: ", message.data, " from the server");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
+
+// 서버와의 연결이 끊겼을 때
 socket.addEventListener("close", () => {
-
+    console.log("Disconnected from Server X");
 });
 
+function handleSubmit(event){
+    // preventDefault() 
+    // 1. a 태그를 눌렀을때도 href 링크로 이동하지 않게 할 경우
+    // 2. form 안에 submit 역할을 하는 버튼을 눌렀어도 새로 실행하지 않게 하고싶을 경우(submit은 작동 됨)
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    console.log(input.value);
+
+    const li = document.createElement("li");
+    li.innerText = `You : ${input.value}`;
+    messageList.append(li);
+
+    input.value="";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault()
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -18,22 +18,46 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
-
 
 // server.js에 있는 socket은 연결된 브라우저를 뜻함
 // function handleConnection(socket) {
 //     console.log(socket);
 
 // } 
-
+const wss = new WebSocket.Server({ server });
 // on 메소드는 event가 동작하는것을 기다린다.
 // on 메소드는 backend에 연결된 사람의 정보를 제공해줌
 // socket에서 온다
 // socket 은 서버와 브라우저 사이의 연결
+const sockets = [];
+
 wss.on("connection", (socket) => {
-    socket.send("Hello!!");
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+    console.log("connected to Browser");
+    socket.on("close", onCloseSocket)
+
+    // 브라우저가 서버에 메세지를 보냈을 때를 위한 listener
+    socket.on("message", (msg) => {
+        const messageStr = msg.toString('utf8')
+        const message = JSON.parse(messageStr)
+        console.log(message);
+
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.pa}`))
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+    });
 })
+
+function onCloseSocket(){
+    console.log("Disconnected from Browser X");
+}
+
+
 
 server.listen(3000, handleListen);
 
